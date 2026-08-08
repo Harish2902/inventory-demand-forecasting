f95361dc8fdfc952b3c98fb64f6408f3d74092f7
Harish_Raghav <143773862+Harish2902@users.noreply.github.com>
2026-08-08 21:35:45 +0530

    Delete src directory

--- src/main/java/com/harish/inventory_demand_forecasting/exception/ResourceNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.harish.inventory_demand_forecasting.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-
-}
